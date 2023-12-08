416efbdbd741cfe6ef0c65b2489c65f535312949
P <117578336+prathameshnium@users.noreply.github.com>
2023-12-08 13:47:59 +0530

    Update Combine_2400-2182.py
    combine-2400-2182-modified.py

--- Combine_2400-2182.py
+++ Combine_2400-2182.py
@@ -1,17 +1,12 @@
 import pymeasure
 import numpy
 import matplotlib.pyplot as plt
+from time import sleep
 import pyvisa
-#print(pymeasure.__version__)
 from pymeasure.instruments.keithley import Keithley2400
 
 
 rm1 = pyvisa.ResourceManager()
-
-
-#print(dir(pyvisa.ResourceManager()))
-#print(dir(pymeasure.instruments.list_resources))
-
 
 keithley_2182= rm1.open_resource("GPIB::7")
 keithley_2182.write("*rst; status:preset; *cls")
@@ -19,27 +14,35 @@ keithley_2182.write("*rst; status:preset; *cls")
 
 
 
-keithley = Keithley2400("GPIB::4")
+keithley_2400 = Keithley2400("GPIB::4")
+
+sleep(5)
+
 lcur=[]
 lvol=[]
 interval_in_ms = 1
 number_of_readings = 2
 
 
-keithley.apply_current()                # Sets up to source current
-keithley.source_current_range = 1e-3   # Sets the source current range to 1 mA
-keithley.compliance_voltage = 10        # Sets the compliance voltage to 10 V
-keithley.source_current = 0             # Sets the source current to 0 mA
-keithley.enable_source()                # Enables the source output
+keithley_2400.apply_current()                # Sets up to source current
+keithley_2400.source_current_range = 1e-3   # Sets the source current range to 1 mA
+keithley_2400.compliance_voltage = 10        # Sets the compliance voltage to 10 V
+keithley_2400.source_current = 0             # Sets the source current to 0 mA
+keithley_2400.enable_source()             # Enables the source output
 
-keithley.measure_voltage()              # Sets up to measure voltage
+
+sleep(5)
+#keithley_2400.measure_voltage()              # Sets up to measure voltage
 #keithley.measure_current()
 
-#for cur in numpy.linspace(5e-4,10e-4,5) :
 for cur in numpy.linspace(-5e-4,5e-4,15) :
 
 
-    keithley.ramp_to_current(cur)
+
+
+    keithley_2400.ramp_to_current(cur)
+
+    sleep(5)
 
     b=keithley_2182.write("status:measurement:enable 512; *sre 1")
     c=keithley_2182.write("sample:count %d" % number_of_readings)
@@ -50,28 +53,24 @@ for cur in numpy.linspace(-5e-4,5e-4,15) :
     keithley_2182.write("initiate")
     keithley_2182.assert_trigger()
     keithley_2182.wait_for_srq()
+    sleep(10)
     voltages = keithley_2182.query_ascii_values("trace:data?")
-    #print("Average voltage: ", sum(voltages) / len(voltages))
-    #print(voltages)
     keithley_2182.query("status:measurement?")
     keithley_2182.write("trace:clear; feed:control next")
 
     # Ramps the current to 0.5 mA]
-    lcur.append(keithley.current)
+    lcur.append(keithley_2400.current)
     #lvol.append(keithley.voltage)
     v_avr=sum(voltages) / len(voltages)
-    #print(vavg)
 
     lvol.append(v_avr)
 
     print(str(cur) + "   " +str(v_avr))
+    sleep(5)
 
 
 
-   # print(str(keithley.voltage) + "   " +str(keithley.current))
-                # Prints the voltage in Volts
-    #print(keithley.current)
-#print(lcur)
+
 print(cur)
 print(lvol)
 
@@ -82,5 +81,5 @@ plt.show()
 
 
 
-keithley.shutdown()                     # Ramps the current to 0 mA and disables output
+keithley_2400.shutdown()                     # Ramps the current to 0 mA and disables output
 
