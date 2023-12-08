f45dba8c5315d7a6bb6134462d9117cbc48a97bc
P <117578336+prathameshnium@users.noreply.github.com>
2023-12-08 14:05:29 +0530

    Create GPIB_TEST.py
    21-10-22 /GPIB_test

---
+++ GPIB_TEST.py
@@ -0,0 +1,15 @@
+#basic test
+
+import pymeasure
+import numpy
+import matplotlib.pyplot as plt
+from time import sleep
+import pyvisa
+from pymeasure.instruments.keithley import Keithley2400
+
+
+rm = pyvisa.ResourceManager()
+print(rm.list_resources())
+
+#print(dir(pyvisa.ResourceManager()))
+#print(dir(pymeasure.instruments.list_resources))
