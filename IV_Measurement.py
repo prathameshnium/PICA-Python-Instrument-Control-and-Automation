046bd94557af328822e0270ce42e0e035056d7d4
P <117578336+prathameshnium@users.noreply.github.com>
2024-03-11 18:20:48 +0530

    Update IV_Measurement.py
    module1

--- IV_Measurement.py
+++ IV_Measurement.py
@@ -1,43 +1,43 @@
+
 import pymeasure
 import numpy
+import pyvisa
 import matplotlib.pyplot as plt
 #print(pymeasure.__version__)
-from pymeasure.instruments.keithley import Keithley2400
 
 
 
-
-keithley = Keithley2400("GPIB::4")
-lcur=[]
-lvol=[]
-
-keithley.apply_current()                # Sets up to source current
-keithley.source_current_range = 1e-3   # Sets the source current range to 1 mA
-keithley.compliance_voltage = 10        # Sets the compliance voltage to 10 V
-keithley.source_current = 0             # Sets the source current to 0 mA
-keithley.enable_source()                # Enables the source output
-
-keithley.measure_voltage()              # Sets up to measure voltage
-keithley.measure_current()
-
-for cur in numpy.linspace(-5e-4,+5e-4,20) :
-    keithley.ramp_to_current(cur)          # Ramps the current to 0.5 mA]
-    lcur.append(cur)
-    print(cur)
-    #lcur.append(keithley.current)
-    lvol.append(keithley.voltage)
-
-   # print(str(keithley.voltage) + "   " +str(keithley.current))
-                # Prints the voltage in Volts
-    #print(keithley.current)
-
-print(lvol)
-
-plt.plot(numpy.linspace(-5e-4,+5e-4,20),lvol)
-plt.show()
+rm1 = pyvisa.ResourceManager()
 
 
+#print(dir(pyvisa.ResourceManager()))
+#print(dir(pymeasure.instruments.list_resources))
 
 
-keithley.shutdown()                     # Ramps the current to 0 mA and disables output
+keithley = rm1.open_resource("GPIB::7")
+keithley.write("*rst; status:preset; *cls")
 
+
+interval_in_ms = 500
+number_of_readings = 10
+
+b=keithley.write("status:measurement:enable 512; *sre 1")
+c=keithley.write("sample:count %d" % number_of_readings)
+d=keithley.write("trigger:source bus")
+e=keithley.write("trigger:delay %f" % (interval_in_ms / 1000.0))
+f=keithley.write("trace:points %d" % number_of_readings)
+i=keithley.write("trace:feed sense1; feed:control next")
+
+keithley.write("initiate")
+keithley.assert_trigger()
+keithley.wait_for_srq()
+
+voltages = keithley.query_ascii_values("trace:data?")
+print("Average voltage: ", sum(voltages) / len(voltages))
+vavg=sum(voltages) / len(voltages)
+print(vavg)
+
+print(voltages)
+
+keithley.query("status:measurement?")
+keithley.write("trace:clear; feed:control next")
