9b34cf66173a317053c752c9e9cabf1a37664610
Prathameshnium <117578336+prathameshnium@users.noreply.github.com>
2024-03-12 14:22:32 +0530

    Update Pyroelectric.py
    Final working Both Instrument connected

--- Pyroelectric.py
+++ Pyroelectric.py
@@ -1,106 +1,129 @@
-#-------------------------------------------------------------------------------
-# Name:        #interfacing Lakeshore350_Temprature_Controller
+#-----------------------------------------------------------------------
+# Name:       Pyroelectricity measurement  #interfacing Lakeshore350_Temprature_Controller and Keithley 6517B electrometer
 # Purpose:
 #
 # Author:      Ketan
 #
-# Created:    11/3/24
-# Changes_done:   V1.2
-#-------------------------------------------------------------------------------
-
+# Created:    3/3/24
+# Changes_done:   V3
+#-----------------------------------------------------------------------
 
 
 #import visa
-import time
-#from lakeshore import Model350
 import pyvisa
-rm1 = pyvisa.ResourceManager()
-print(f"List of Inst: {rm1.list_resources()}\n")
+import time
+import numpy as np
+import pandas as pd
+#from lakeshore import Model350
+from pymeasure.instruments.keithley import Keithley6517B
 
-temp_controller= rm1.open_resource("GPIB::12")
-time.sleep(0.5)
-filename = 'E:/Prathamesh/Python Stuff/Py Pyroelectric/Test_data/temperature_data.csv'
+
+filename = 'E:/Prathamesh/Python Stuff/Py Pyroelectric/Test_data/Pyro_data.csv'
+T_final=308 #cutoff
 
 try:
 
-    #temp_controller = Model350('GPIB0::12::INSTR')
+    rm1 = pyvisa.ResourceManager()
+    print(rm1.list_resources())
 
-    identification = temp_controller.query('*IDN?')
-    print(f"Instrument ID : {identification}")
+    time.sleep(0.5)
+    temp_controller= rm1.open_resource("GPIB::12")
+    #initilization of Both Instrumnets
+
+    #temp_controller = Model350('GPIB0::1::INSTR')
+
+    #identification = temp_controller.query('*IDN?')
+    #print(f"Instrument identification: {identification}")
     #rm = visa.ResourceManager()
     #temp_controller = rm.open_resource(address)
-    print(f"Connected to {temp_controller.query('*IDN?').strip()}")
+    print(f"\nConnected to {temp_controller.query('*IDN?').strip()}")
     time.sleep(0.5)
+
 
     temp_controller.write('*RST')
-    time.sleep(0.5)
+    time.sleep(0.2)
     temp_controller.write('*CLS')
-    time.sleep(1)
-    temp_controller.write('RAMP 2')
-    #temp_controller.write('HTRSET 1,1,1,0,1')
-    temp_controller.write('SETP 1,310')
-    time.sleep(2)
-    #temp_controller.write('SETP 2,305')
-
-    temp_controller.write('CLIMIT 1, 310.0, 10, 0')
-    time.sleep(2)
-    #temp_controller.write("INSET 305")
-    temp_controller.write('RANGE 4')
-    #temp_controller.write('SETP 2,305')
-
-
-    time.sleep(2)
-    #print("Heating data" +str(temp_controller.query('HTRSET?')))
-    time.sleep(2)
-    #print("Heating data 1" +str(temp_controller.query('SETP?')))
-
+    time.sleep(0.2)
+    temp_controller.write('RAMP 1, 1, 1')
     time.sleep(0.5)
+    temp_controller.write('RANGE 2') # 3 is 1W , 4 is 100 W
+    time.sleep(0.5)
+    temp_controller.write('SETP 1,305')
+    time.sleep(0.5)
+    #temp_controller.write('SETP 2,305')
+    time.sleep(0.5)
+    temp_controller.write('CLIMIT 1, 305.0, 10, 0')
+    time.sleep(0.5)
+    #temp_controller.write('CLIMIT 1, 307')
 
-    #for ramping the temprature 1K/Min
 
-    #temp_controller.write('RAMP 1')
+    #---------------------------------------
 
+    keithley = Keithley6517B("GPIB0::27::INSTR")
+    time.sleep(1)
+    print(f"\nConnected to {keithley.id}")
+    print(f"\n-----------------------------------------------------\n")
+
+    time.sleep(1)
+    #keithley.apply_current()  # Sets up to source current
+    #keithley.current_range = 10e-3  # Sets the source current range to 10 mA
+    #keithley.compliance_voltage = 10  # Sets the compliance voltage to 10 V
+    #keithley.enable_source()  # Enables the source output
+    keithley.measure_current()
+
+    #------------------------------------
+
+
+
+    #filename = 'temperature_data.txt'
     with open(filename, 'w') as file:
-        file.write("Time (s),Temperature (K)\n")
+        file.write("Time (s),Temperature (K),Current (A)\n")
 
 except Exception as e:
-    print(f"Initialization error with Lakeshore  : {e}")
+    print(f"Initialization error : {e}")
 
 
 def main():
-    #instrument_address = 'GPIB0::1::INSTR'  # Replace with actual address
-    #temp_controller = initialize_temperature_controller(instrument_address)
-
 
     try:
         start_time = time.time()
-        while True:
+        global Check
+        Check=True
+        while Check:
             elapsed_time = time.time() - start_time
-            temperature = temp_controller.query('KRDG? A').strip()
-            print(f"Time: {elapsed_time:.2f} s\t\t|\t\t Temperature: {temperature} K")
-            if float(temperature)>302.5:
-                temp_controller.write('RANGE 0')
-                time.sleep(0.5)
-                print("T larger than 302.5")
-                break
+            temperature = temp_controller.query('KRDG? A').strip() # Temperature in K
+            current = keithley.current  # Read current in Amps
 
 
-            #print(float(temperature)>302)
+            print(f"Time: {elapsed_time:.2f} s, Temperature: {temperature} K,Current: {current}")
 
             with open(filename, 'a') as file:
-                file.write(f"{elapsed_time:.2f},{temperature}\n")
+                file.write(f"{elapsed_time:.2f},{temperature},{current}\n")
+            if float(temperature)>T_final:
+                temp_controller.write('RANGE 0')
+                time.sleep(0.1)
+                print(f"T larger than {T_final}")
+                Check=False
 
-            time.sleep(2)
+            time.sleep(0.5) #old 0.2
 
     except Exception as e:
-        print(f"error with Lakeshore  : {e}")
+        print(f"error : {e}")
     except KeyboardInterrupt:
         temp_controller.write('RANGE 0')
-        time.sleep(1)
-        temp_controller.close()
         time.sleep(0.5)
-        print("\n\nLakeshore closed")
-        print("\n Measurement stopped by User ")
+        print("\n Measurement stopped ")
+        temp_controller.close()
+        print("Lakeshore closed")
+        time.sleep(0.2)
+        keithley.clear()
+        #keithley.reset()
+
+        time.sleep(1)
+        keithley.shutdown()  # Ramps the current to 0 mA and disables output
+        print("keithley closed")
+
+
 
 
 if __name__ == "__main__":
