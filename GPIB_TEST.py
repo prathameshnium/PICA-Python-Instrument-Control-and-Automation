e19f81570e604d72be0da75e6bc80c28a6a00258
Prathamesh K. Deshmukh <117578336+prathameshnium@users.noreply.github.com>
2025-09-07 01:27:09 +0530

    Update GPIB_TEST.py

--- GPIB_TEST.py
+++ GPIB_TEST.py
@@ -1,13 +1,34 @@
-#basic test
-
-#import pymeasure
-from time import sleep
+#-------------------------------------------------------------------------------
+# Name:        GPIB Test
+# Purpose:
+#
+# Author:      Instrument-DSL
+#
+# Created:     04/03/2024
+# Copyright:   (c) Instrument-DSL 2024
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
 import pyvisa
-#from pymeasure.instruments.keithley import Keithley2400
+rm1 = pyvisa.ResourceManager()
+print(rm1.list_resources())
+
+temp_controller= rm1.open_resource("GPIB0::13::INSTR")
+print(f"ID is : {temp_controller.query('*IDN?')}")
+
+"""
+print(f"ID is : {temp_controller.query('*IDN?')}")
+print(temp_controller.query('INTYPE?'))
+
+temp_controller.write('RAMP 2')
+
+print(temp_controller.query('KRDG? B').strip())
+print(temp_controller.query("HTR?"))
+
+temp_controller.write('HTRSET 2,1,2,0,1')
+
+temp_controller.write("RAMP 1,1,10.5")
 
 
-rm = pyvisa.ResourceManager()
-print(rm.list_resources())
+print(temp_controller.query("HTRST?"))
 
-#print(dir(pyvisa.ResourceManager()))
-#print(dir(pymeasure.instruments.list_resources))
+"""
